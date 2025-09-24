3622b6d7c33321c6eb6a5ccb45498e908e4a2447
Nicolas Merget <104347736+nmerget@users.noreply.github.com>
2025-09-24 12:33:03 +0200

    Feat angular 19 update (#2160)
    * feat: update angular from v18 to v19
    
    * chore: removed 18 naming

---
+++ angular-example/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {DBButton, DBCustomSelect} from '@db-ux/ngx-core-components';
+
+@Component({
+  selector: 'app-root',
+  imports: [DBButton, DBCustomSelect],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'angular-example';
+}
