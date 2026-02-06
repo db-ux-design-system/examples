716a62b59dc49a23da0b2faf2341372c1073a548
Nicolas Merget <nicolas.merget@deutschebahn.com>
2026-02-06 12:16:20 +0100

    test: revert App.tsx to avoid bias

--- react-example/src/App.tsx
+++ react-example/src/App.tsx
@@ -1,9 +1,32 @@
-import { DBLoadingIndicator } from "@db-ux/react-core-components";
+import { DBButton } from "@db-ux/react-core-components";
 
 const App = () => {
   return (
     <main>
-      <DBLoadingIndicator>Loading</DBLoadingIndicator>
+      <div
+        data-density="functional"
+        className="db-color-informational container-with-padding"
+      >
+        <h1>db-ux-functional</h1>
+        <p>Use this as default for enterprise apps</p>
+        <DBButton icon="plus">Test</DBButton>
+      </div>
+      <div
+        data-density="regular"
+        className="db-color-successful container-with-padding"
+      >
+        <h1>db-ux-regular</h1>
+        <p>Use this as default for consumer apps</p>
+        <DBButton icon="plus">Test</DBButton>
+      </div>
+      <div
+        data-density="expressive"
+        className="db-color-successful db-bg-color-basic-level-2 container-with-padding"
+      >
+        <h1>db-ux-expressive</h1>
+        <p>Use this as default for marketing apps</p>
+        <DBButton icon="plus">Test</DBButton>
+      </div>
     </main>
   );
 };
